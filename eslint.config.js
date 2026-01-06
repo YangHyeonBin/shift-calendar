82feb81752435f0e621e653a8d4c4466e64e91da
Yang HyeonBin <idgusqls0506@naver.com>
2026-01-06 20:56:21 +0900

    chore: eslint, prettier 세팅

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-config-prettier";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: { jsx: true },
+            },
+        },
+        plugins: {
+            "@typescript-eslint": typescript,
+            react,
+            "react-hooks": reactHooks,
+            import: importPlugin,
+        },
+        rules: {
+            // TypeScript
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { argsIgnorePattern: "^_" },
+            ],
+            "@typescript-eslint/no-explicit-any": "warn",
+
+            // React
+            "react/react-in-jsx-scope": "off",
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+
+            // Import 정렬
+            "import/order": [
+                "warn",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                    ],
+                    "newlines-between": "always",
+                },
+            ],
+        },
+        settings: {
+            react: { version: "detect" },
+        },
+    },
+    prettier, // Prettier와 충돌하는 규칙 비활성화
+    {
+        ignores: ["node_modules", "build", ".react-router"],
+    },
+];
